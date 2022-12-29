d43c35441832e90cfb6243e318dcf7c32eb20c48
Bogdan <c0pland@icloud.com>
2022-12-29 21:42:42 +0200

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Богдан Беннер on 29.12.22.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
