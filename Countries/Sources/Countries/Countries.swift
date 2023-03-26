829cbbd397ae23cdf4e36c8928b628b43db74936
Bogdan <c0pland@icloud.com>
2023-03-26 12:21:58 +0300

    Add Package.swift

---
+++ Countries/Sources/Countries/Countries.swift
@@ -0,0 +1,8 @@
+@main
+public struct Countries {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(Countries().text)
+    }
+}
---
+++ Countries/Tests/CountriesTests/CountriesTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Countries
+
+final class CountriesTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Countries().text, "Hello, World!")
+    }
+}
