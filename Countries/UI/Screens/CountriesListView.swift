51808a96e7eb614ac5944f91dcbf1614f0a62aa5
Bogdan Benner <c0pland@icloud.com>
2023-04-18 17:05:22 +0300

    Add search for countries

--- Countries/UI/Screens/CountriesListView.swift
+++ Countries/UI/Screens/CountriesListView.swift
@@ -11,14 +11,17 @@ struct CountriesListView: View {
 	@EnvironmentObject private var countryViewModel: CountryViewModel
 	@ObservedObject var favoriteCountriesViewModel = FavoriteCountriesViewModel()
 	@EnvironmentObject private var router: Router
+	@State private var searchText = ""
 	
 	var body: some View {
-		List(countryViewModel.countries) { country in
-			CountryListCell(country: country)
-				.onTapGesture {
-					router.countriesPath.append(country)
-				}
-				.id(ScrollAnchor.countries)
+		List {
+			ForEach(searchResults) { country in
+				CountryListCell(country: country)
+					.onTapGesture {
+						router.countriesPath.append(country)
+					}
+					.id(ScrollAnchor.countries)
+			}
 		}
 		.navigationTitle(router.navigationTitle)
 		.navigationDestination(for: Country.self) { country in
@@ -26,6 +29,20 @@ struct CountriesListView: View {
 		}
 		.navigationDestination(for: Union.self) { union in
 			UnionDetailView(union: union)
+		}
+		.searchable(text: $searchText)
+	}
+	
+	var searchResults: [Country] {
+		if searchText.isEmpty {
+			return countryViewModel.countries
+		} else {
+			return countryViewModel.countries.filter { country in
+				country.name.common.contains(searchText) ||
+				country.name.official.contains(searchText) ||
+				country.capital.contains(searchText) // ||
+				// ((country.currencies.first?.value.name.contains(searchText)) != nil)
+			}
 		}
 	}
 }
